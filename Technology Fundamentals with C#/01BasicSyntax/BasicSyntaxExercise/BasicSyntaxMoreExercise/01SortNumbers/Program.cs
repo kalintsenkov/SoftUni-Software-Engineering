bb273e1ea0ddb05df159d33c3445ee8307af36fe
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-04-02 18:15:35 +0300

    Update 01SortNumbers code

--- Technology Fundamentals with C#/01BasicSyntax/BasicSyntaxExercise/BasicSyntaxMoreExercise/01SortNumbers/Program.cs
+++ Technology Fundamentals with C#/01BasicSyntax/BasicSyntaxExercise/BasicSyntaxMoreExercise/01SortNumbers/Program.cs
@@ -10,7 +10,14 @@ namespace _01SortNumbers
             int secondNumber = int.Parse(Console.ReadLine());
             int thirdNumber = int.Parse(Console.ReadLine());
 
-            
+            int[] result = { firstNumber, secondNumber, thirdNumber };
+            Array.Sort(result);
+            Array.Reverse(result);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
         }
     }
 }
