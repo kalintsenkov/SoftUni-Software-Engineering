741642c8b9b7a3e6b84ce2fcacc8776e3c74cb35
kalintsenkov <kalintsenkov3@gmail.com>
2019-04-06 19:11:21 +0300

    Add Demo-Final-Exam-06-April-2019 folder

---
+++ Technology Fundamentals with C#/OldFinalExams/Demo-Final-Exam-06-April-2019/02Deciphering/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02Deciphering
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string letters = Console.ReadLine();
+
+            string pattern = @"^[d-z{},|#]+$";
+            Regex regex = new Regex(pattern);
+
+            StringBuilder decryptedText = new StringBuilder();
+
+            if (regex.IsMatch(input))
+            {
+                DecryptingText(input, decryptedText);
+
+                var substrings = letters.Split(" ").ToArray();
+
+                var firstSubstring = substrings[0];
+                var secondSubstring = substrings[1];
+
+                decryptedText = decryptedText.Replace(firstSubstring, secondSubstring);
+            }
+            else
+            {
+                Console.WriteLine($"This is not the book you are looking for.");
+                return;
+            }
+
+            Console.WriteLine(decryptedText);
+        }
+
+        public static void DecryptingText(string input, StringBuilder decryptedText)
+        {
+            foreach (var character in input)
+            {
+                char decryptedChar = (char)(character - 3);
+
+                decryptedText.Append(decryptedChar);
+            }
+        }
+    }
+}
