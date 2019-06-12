348663da5d49bfcbc7ad0a6911f342dde397e34c
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-06-12 17:47:16 +0300

    Update StartUp.cs

--- C#-Advanced/Homeworks-And-Labs/07WorkshopLab/CustomStructures/StartUp.cs
+++ C#-Advanced/Homeworks-And-Labs/07WorkshopLab/CustomStructures/StartUp.cs
@@ -6,21 +6,21 @@ namespace CustomStructures
     {
         public static void Main()
         {
-            //var list = new CustomList();
+            var list = new CustomList();
 
-            //list.Add(1);
-            //list.Add(3);
-            //list.Add(5);
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
 
-            //list.AddRange(new int[] { 11, 21, 52, 33, 45, 65, 91, 71 });
-            //list.RemoveAt(3);
-            ////list.InsertAt(100, 51);
-            //list.InsertAt(4, 8);
-            //list.Swap(0, 8);
+            list.AddRange(new int[] { 11, 21, 52, 33, 45, 65, 91, 71 });
+            list.RemoveAt(3);
+            
+            //list.InsertAt(100, 51);
+            
+            list.InsertAt(4, 8);
+            list.Swap(0, 8);
 
-            //list.ForEach(Console.WriteLine);
-
-
+            list.ForEach(Console.WriteLine);
         }
     }
 }
