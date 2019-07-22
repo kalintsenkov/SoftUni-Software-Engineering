9a8f545844d906afc78273d01c128c3100117e4e
Kalin Tsenkov <kalintsenkov3@gmail.com>
2019-07-22 15:59:45 +0300

    Add CommandInterpreter

---
+++ CSharp-OOP/Homeworks-And-Labs/CommandPattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    using Contracts;
+
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Hello, {args[0]}";
+        }
+    }
+}
---
+++ CSharp-OOP/Homeworks-And-Labs/CommandPattern/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CommandPattern
+{
+    using Core;
+    using Core.Contracts;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ICommandInterpreter command = new CommandInterpreter();
+            IEngine engine = new Engine(command);
+
+            engine.Run();
+        }
+    }
+}
