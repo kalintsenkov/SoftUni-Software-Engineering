613cee3db2ee278295bf7b9f7352f6ebcec08149
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-17 18:59:42 +0300

    Update 08CreateTableUsers.sql

--- CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/08CreateTableUsers.sql
+++ CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/08CreateTableUsers.sql
@@ -9,7 +9,7 @@ CREATE TABLE Users (
 
 INSERT INTO Users (Username, [Password], ProfilePicture, LastLoginTime, IsDeleted) 
 VALUES		('Kiro Kirkov', '1234567', 2, '1989-05-20', 0),
-			('Ivan Ivanov', '1234567', 2, '1969-02-21', 1),
-			('Pesho Peshov', '1234567', 1, '1999-05-16', 1),
-			('Stamat Stamatov', '1234567', 1, '1976-02-09', 0),
-			('Vanko Vankov', '1234567', 2 , '1999-05-05', 0)
+		('Ivan Ivanov', '1234567', 2, '1969-02-21', 1),
+		('Pesho Peshov', '1234567', 1, '1999-05-16', 1),
+		('Stamat Stamatov', '1234567', 1, '1976-02-09', 0),
+		('Vanko Vankov', '1234567', 2 , '1999-05-05', 0)
