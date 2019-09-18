2f58025fd06b17a4927a1e494516df5fee221a83
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-18 11:08:23 +0300

    Update 16CreateSoftUniDatabase.sql

--- CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/16CreateSoftUniDatabase.sql
+++ CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/16CreateSoftUniDatabase.sql
@@ -28,23 +28,23 @@ CREATE TABLE Employees (
 
 INSERT INTO Towns([Name])
 VALUES		('Sofia'),
-			('Plovdiv'),
-			('Varna'),
-			('Burgas')
+		('Plovdiv'),
+		('Varna'),
+		('Burgas')
 
 INSERT INTO Departments([Name])
 VALUES		('Engineering'),
-			('Sales'),
-			('Marketing'),
-			('Software Development'),
-			('Quality Assurance')
+		('Sales'),
+		('Marketing'),
+		('Software Development'),
+		('Quality Assurance')
 
 INSERT INTO Employees
-			(FirstName, MiddleName, LastName,
-			JobTitle, DepartmentId, HireDate,
-			Salary, AddressId)
+		(FirstName, MiddleName, LastName,
+		JobTitle, DepartmentId, HireDate,
+		Salary, AddressId)
 VALUES		('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00, NULL),
-			('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00, NULL),
-			('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25, NULL),
-			('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00, NULL),
-			('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88, NULL)
+		('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00, NULL),
+		('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25, NULL),
+		('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00, NULL),
+		('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88, NULL)
