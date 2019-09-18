dcd3ff9c2eeb6879253153a2ad6ed2b45723ba46
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-18 11:06:52 +0300

    Update 21BasicSelectSomeFieldsAndOrderThem.sql

--- CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/21BasicSelectSomeFieldsAndOrderThem.sql
+++ CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/21BasicSelectSomeFieldsAndOrderThem.sql
@@ -7,8 +7,8 @@ FROM	Departments
 ORDER BY [Name]
 
 SELECT	[FirstName], 
-		[LastName], 
-		[JobTitle], 
-		[Salary] 
+	[LastName], 
+	[JobTitle], 
+	[Salary] 
 FROM	Employees 
 ORDER BY Salary DESC
