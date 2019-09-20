c15bef5160c5758dc02caf694e94ac5d89311609
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-20 19:17:15 +0300

    Update 07CreateTablePeople.sql

--- CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/07CreateTablePeople.sql
+++ CSharp-DB/Databases-Basics/01DataDefinitionAndDatatypes/07CreateTablePeople.sql
@@ -10,8 +10,8 @@ CREATE TABLE People (
 )
 
 INSERT INTO People ([Name], Picture, Height, [Weight], Gender, Birthdate, Biography)
- VALUES		('Kiro Kirkov', 2, 1.85, 35.5, 'm', '1989-05-20', 'Zdr'),
-		('Ivan Ivanov', 1, NULL, 40.0, 'm', '1969-02-21', 'Zdrrr'),
-		('Pesho Peshov', 2, 2.10, NULL, 'm', '1999-05-16', NULL),
-		('Stamat Stamatov', 1, 1.93, 81.0, 'm', '1976-02-09', 'Zdr'),
-		('Vanko Vankov', 1, 1.71, 75.8, 'm', '1999-05-05', 'Zdr')
+VALUES	    ('Kiro Kirkov', 2, 1.85, 35.5, 'm', '1989-05-20', 'Zdr'),
+	    ('Ivan Ivanov', 1, NULL, 40.0, 'm', '1969-02-21', 'Zdrrr'),
+	    ('Pesho Peshov', 2, 2.10, NULL, 'm', '1999-05-16', NULL),
+	    ('Stamat Stamatov', 1, 1.93, 81.0, 'm', '1976-02-09', 'Zdr'),
+	    ('Vanko Vankov', 1, 1.71, 75.8, 'm', '1999-05-05', 'Zdr')
