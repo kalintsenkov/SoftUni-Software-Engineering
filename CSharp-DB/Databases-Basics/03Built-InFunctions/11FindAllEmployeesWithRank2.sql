c0626dc947176bab43a3bb54a56b0095c1320179
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-23 23:24:55 +0300

    Update 11FindAllEmployeesWithRank2.sql

--- CSharp-DB/Databases-Basics/03Built-InFunctions/11FindAllEmployeesWithRank2.sql
+++ CSharp-DB/Databases-Basics/03Built-InFunctions/11FindAllEmployeesWithRank2.sql
@@ -1,13 +1,13 @@
   SELECT R.EmployeeID,
          R.FirstName,
-	     R.LastName,
-	     R.Salary,
-	     R.[Rank]
+	 R.LastName,
+	 R.Salary,
+	 R.[Rank]
     FROM (SELECT EmployeeID,
-			     FirstName,
-	             LastName,
-	             Salary,
-	             DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+	         FirstName,
+	         LastName,
+	         Salary,
+	         DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
             FROM Employees
            WHERE Salary BETWEEN 10000 AND 50000) AS R
    WHERE [Rank] = 2
