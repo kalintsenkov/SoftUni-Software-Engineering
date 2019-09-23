2b305f242761df112d7662f73ca5d7ec693ea8b7
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-23 23:29:03 +0300

    Update 19PeopleTable.sql

--- CSharp-DB/Databases-Basics/03Built-InFunctions/19PeopleTable.sql
+++ CSharp-DB/Databases-Basics/03Built-InFunctions/19PeopleTable.sql
@@ -8,12 +8,12 @@ CREATE TABLE People
 INSERT INTO People([Name], Birthdate)
 VALUES      ('Victor', '2000-12-07'),
             ('Steven', '1992-09-10'),
-			('Stephen', '1910-09-19'),
-			('John', '2010-01-06')
+	    ('Stephen', '1910-09-19'),
+	    ('John', '2010-01-06')
 
 SELECT [Name],
-	   DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years],
-	   DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months],
-	   DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days],
-	   DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutes]
+       DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years],
+       DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months],
+       DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days],
+       DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutes]
   FROM People
