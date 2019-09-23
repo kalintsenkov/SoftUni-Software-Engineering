c73e941effce54ecc60ea1d67596315af2bcc3c8
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-23 23:22:56 +0300

    Update 03FindFirstNamesOfAllEmployess.sql

--- CSharp-DB/Databases-Basics/03Built-InFunctions/03FindFirstNamesOfAllEmployess.sql
+++ CSharp-DB/Databases-Basics/03Built-InFunctions/03FindFirstNamesOfAllEmployess.sql
@@ -1,4 +1,4 @@
 SELECT FirstName
   FROM Employees
  WHERE DepartmentID IN (3, 10) AND 
-	   HireDate BETWEEN CAST('1995' AS DATE) AND CAST('2006' AS DATE)
+       HireDate BETWEEN CAST('1995' AS DATE) AND CAST('2006' AS DATE)
