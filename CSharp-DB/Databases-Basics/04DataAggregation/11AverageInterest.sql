c7e8b7f9d5bc3dc90d66a9e471a6e629dba086dd
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-27 18:23:06 +0300

    Update 11AverageInterest.sql

--- CSharp-DB/Databases-Basics/04DataAggregation/11AverageInterest.sql
+++ CSharp-DB/Databases-Basics/04DataAggregation/11AverageInterest.sql
@@ -3,6 +3,7 @@
          AVG(DepositInterest) AS [AverageInterest]
     FROM WizzardDeposits
    WHERE DepositStartDate > '1985-01-01'
-GROUP BY DepositGroup, IsDepositExpired
+GROUP BY DepositGroup, 
+         IsDepositExpired
 ORDER BY DepositGroup DESC,
          IsDepositExpired ASC
