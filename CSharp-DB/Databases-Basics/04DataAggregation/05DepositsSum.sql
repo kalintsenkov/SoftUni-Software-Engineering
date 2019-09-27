8d7d782fa2673a7dc457ef9f3568ec93f1fbb418
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-09-27 13:24:59 +0300

    Update 05DepositsSum.sql

--- CSharp-DB/Databases-Basics/04DataAggregation/05DepositsSum.sql
+++ CSharp-DB/Databases-Basics/04DataAggregation/05DepositsSum.sql
@@ -1,4 +1,4 @@
-SELECT w.DepositGroup,
-	   SUM(w.DepositAmount) AS [TotalSum]
-  FROM WizzardDeposits AS w
+  SELECT w.DepositGroup,
+	 SUM(w.DepositAmount) AS [TotalSum]
+    FROM WizzardDeposits AS w
 GROUP BY w.DepositGroup
