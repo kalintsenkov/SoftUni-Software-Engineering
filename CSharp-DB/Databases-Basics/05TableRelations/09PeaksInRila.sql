1cc9c90d62c482bcb2797de981db2b8caf5745f2
Kalin Tsenkov <kalintsenkov3@gmail.com>
2019-10-01 10:22:38 +0300

    Add solutions to 05TableRelations

---
+++ CSharp-DB/Databases-Basics/05TableRelations/09PeaksInRila.sql
@@ -0,0 +1,8 @@
+  SELECT m.MountainRange,
+         p.PeakName,
+	     p.Elevation
+    FROM Mountains AS m
+    JOIN Peaks AS p
+      ON m.Id = p.MountainId
+   WHERE m.MountainRange = 'Rila'
+ORDER BY p.Elevation DESC
