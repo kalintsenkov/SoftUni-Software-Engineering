215b6bbd95f2068ac77eb57575ebafca9e939906
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-01 10:25:33 +0300

    Update 04Self-Referencing.sql

--- CSharp-DB/Databases-Basics/05TableRelations/04Self-Referencing.sql
+++ CSharp-DB/Databases-Basics/05TableRelations/04Self-Referencing.sql
@@ -11,8 +11,8 @@ CREATE TABLE Teachers
 
 INSERT INTO Teachers(TeacherID, [Name], ManagerID)
 VALUES      (101, 'John', NULL),
-			(102, 'Maya', 106),
-			(103, 'Silvia', 106),
-			(104, 'Ted', 105),
-			(105, 'Mark', 101),
-			(106, 'Greta', 101)
+            (102, 'Maya', 106),
+            (103, 'Silvia', 106),
+            (104, 'Ted', 105),
+            (105, 'Mark', 101),
+            (106, 'Greta', 101)
