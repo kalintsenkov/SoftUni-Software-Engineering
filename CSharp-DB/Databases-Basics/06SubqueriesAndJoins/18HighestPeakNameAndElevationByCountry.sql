5d57263d1283028dfea008fb28ecea5c19c63f23
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-03 21:27:53 +0300

    Update 18HighestPeakNameAndElevationByCountry.sql

--- CSharp-DB/Databases-Basics/06SubqueriesAndJoins/18HighestPeakNameAndElevationByCountry.sql
+++ CSharp-DB/Databases-Basics/06SubqueriesAndJoins/18HighestPeakNameAndElevationByCountry.sql
@@ -1,16 +1,17 @@
-   SELECT TOP(5) k.Country,
-		  ISNULL(k.PeakName, '(no highest peak)') 
-	   AS [Highest Peak Name],
-		  ISNULL(k.[Highest Peak Elevation], 0) 
-	   AS [Highest Peak Elevation],
-		  ISNULL(k.MountainRange, '(no mountain)') 
-	   AS [Mountain]
+   SELECT TOP(5) 
+          k.Country,
+          ISNULL(k.PeakName, '(no highest peak)') 
+       AS [Highest Peak Name],
+	  ISNULL(k.[Highest Peak Elevation], 0) 
+       AS [Highest Peak Elevation],
+          ISNULL(k.MountainRange, '(no mountain)') 
+       AS [Mountain]
      FROM (   SELECT c.CountryName AS [Country],
-           		     p.PeakName,
+           	     p.PeakName,
                      MAX(p.Elevation) AS [Highest Peak Elevation],
-           		     m.MountainRange,
-           		     DENSE_RANK() OVER (PARTITION BY c.CountryName ORDER BY MAX(p.Elevation) DESC)
-           	      AS [Rank]
+           	     m.MountainRange,
+           	     DENSE_RANK() OVER (PARTITION BY c.CountryName ORDER BY MAX(p.Elevation) DESC)
+           	  AS [Rank]
                 FROM Countries AS c
            LEFT JOIN MountainsCountries AS mc
                   ON mc.CountryCode = c.CountryCode
@@ -19,7 +20,7 @@
            LEFT JOIN Peaks AS p
                   ON p.MountainId = m.Id
             GROUP BY c.CountryName,
-           		     p.PeakName,
-           		     m.MountainRange) AS k
+           	     p.PeakName,
+           	     m.MountainRange) AS k
     WHERE k.[Rank] = 1
  ORDER BY k.Country, [Highest Peak Name]
