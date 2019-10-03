fcd88a5dc4e3b562582990592346264801051e1d
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-03 17:42:48 +0300

    Update 03SalesEmployees.sql

--- CSharp-DB/Databases-Basics/06SubqueriesAndJoins/03SalesEmployees.sql
+++ CSharp-DB/Databases-Basics/06SubqueriesAndJoins/03SalesEmployees.sql
@@ -1,7 +1,7 @@
   SELECT e.EmployeeID,
          e.FirstName,
-		 e.LastName,
-		 d.[Name] AS [DepartmentName]
+         e.LastName,
+         d.[Name] AS [DepartmentName]
     FROM Employees AS e
     JOIN Departments AS d
       ON d.DepartmentID = e.DepartmentID
