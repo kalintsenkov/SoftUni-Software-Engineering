6d7ab881d7b5bb6407efe826491061c97f1ef827
Kalin Tsenkov <kalintsenkov3@gmail.com>
2019-10-03 21:24:12 +0300

    Add solutions to 06SubqueriesAndJoins

---
+++ CSharp-DB/Databases-Basics/06SubqueriesAndJoins/15ContinentsAndCurrencies.sql
@@ -0,0 +1,17 @@
+  SELECT k.ContinentCode,
+         k.CurrencyCode,
+         k.CurrencyUsage
+    FROM (  SELECT [cont].ContinentCode,
+                   [count].CurrencyCode,
+          		   COUNT([count].CurrencyCode)
+          	    AS [CurrencyUsage],
+          	       DENSE_RANK() OVER (PARTITION BY [cont].ContinentCode ORDER BY COUNT([count].CurrencyCode) DESC)
+                AS [Rank]
+              FROM Countries AS [count]
+              JOIN Continents AS [cont]
+                ON [cont].ContinentCode = [count].ContinentCode
+          GROUP BY [cont].ContinentCode,
+                   [count].CurrencyCode) AS k
+   WHERE k.[Rank] = 1
+     AND k.CurrencyUsage != 1
+ORDER BY k.ContinentCode
