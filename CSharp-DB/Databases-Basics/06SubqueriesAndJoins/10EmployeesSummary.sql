500bd0022e868c225d15cd924150a19c40c2d09d
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-03 17:44:37 +0300

    Update 10EmployeesSummary.sql

--- CSharp-DB/Databases-Basics/06SubqueriesAndJoins/10EmployeesSummary.sql
+++ CSharp-DB/Databases-Basics/06SubqueriesAndJoins/10EmployeesSummary.sql
@@ -1,9 +1,9 @@
   SELECT TOP(50) emp.EmployeeID,
-		 emp.FirstName + ' ' + emp.LastName
-	  AS [EmployeeName],
-		 mng.FirstName + ' ' + mng.LastName
-	  AS [ManagerName],
-		 dep.[Name] AS [DepartmentName]
+	 emp.FirstName + ' ' + emp.LastName
+      AS [EmployeeName],
+         mng.FirstName + ' ' + mng.LastName
+      AS [ManagerName],
+	 dep.[Name] AS [DepartmentName]
     FROM Employees AS emp
     JOIN Employees AS mng
       ON mng.EmployeeID = emp.ManagerID
