dafa1cd669be660d07d36c9b0d4569adafaa25c1
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-03 17:42:28 +0300

    Update 02AddressesWithTowns.sql

--- CSharp-DB/Databases-Basics/06SubqueriesAndJoins/02AddressesWithTowns.sql
+++ CSharp-DB/Databases-Basics/06SubqueriesAndJoins/02AddressesWithTowns.sql
@@ -1,7 +1,7 @@
   SELECT TOP(50) e.FirstName,
          e.LastName,
   	     t.[Name] AS [Town],
-  	      a.AddressText
+  	     a.AddressText
     FROM Employees AS e
     JOIN Addresses AS a
       ON a.AddressID = e.AddressID
