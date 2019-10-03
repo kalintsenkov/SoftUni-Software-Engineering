4f7c2a7578ef6acd98edd72058a9c85d503a8dae
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-03 21:26:01 +0300

    Update 15ContinentsAndCurrencies.sql

--- CSharp-DB/Databases-Basics/06SubqueriesAndJoins/15ContinentsAndCurrencies.sql
+++ CSharp-DB/Databases-Basics/06SubqueriesAndJoins/15ContinentsAndCurrencies.sql
@@ -3,8 +3,7 @@
          k.CurrencyUsage
     FROM (  SELECT [cont].ContinentCode,
                    [count].CurrencyCode,
-          		     COUNT([count].CurrencyCode)
-          	    AS [CurrencyUsage],
+          		     COUNT([count].CurrencyCode) AS [CurrencyUsage],
           	       DENSE_RANK() OVER (PARTITION BY [cont].ContinentCode ORDER BY COUNT([count].CurrencyCode) DESC)
                 AS [Rank]
               FROM Countries AS [count]
