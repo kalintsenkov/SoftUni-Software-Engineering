583bfd9c618d6f17bdd3ac6bfd2919128c7e4554
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-06 18:51:58 +0300

    Update 02Insert.sql

--- CSharp-DB/Databases-Basics/Exams/Exam16April2019/02Insert.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam16April2019/02Insert.sql
@@ -1,13 +1,13 @@
 INSERT INTO Planes([Name], Seats, [Range])
-VALUES		('Airbus 336', 112, 5132),
-			('Airbus 330', 432, 5325),
-			('Boeing 369', 231, 2355),
-			('Stelt 297', 254, 2143),
-			('Boeing 338', 165, 5111),
-			('Airbus 558', 387, 1342),
-			('Boeing 128', 345, 5541)
+VALUES	          ('Airbus 336', 112, 5132),
+	          ('Airbus 330', 432, 5325),
+	          ('Boeing 369', 231, 2355),
+	          ('Stelt 297', 254, 2143),
+	          ('Boeing 338', 165, 5111),
+	          ('Airbus 558', 387, 1342),
+	          ('Boeing 128', 345, 5541)
 
 INSERT INTO LuggageTypes([Type])
-VALUES		('Crossbody Bag'),
-			('School Backpack'),
-			('Shoulder Bag')
+VALUES	      ('Crossbody Bag'),
+	      ('School Backpack'),
+	      ('Shoulder Bag')
