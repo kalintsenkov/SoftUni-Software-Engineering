63dc28bf766e362e6fc0e78385d000dc117cf911
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-06 18:55:27 +0300

    Update 13CountOfTrips.sql

--- CSharp-DB/Databases-Basics/Exams/Exam16April2019/13CountOfTrips.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam16April2019/13CountOfTrips.sql
@@ -1,6 +1,6 @@
   SELECT p.FirstName,
          p.LastName,
-		 COUNT(t.FlightId) AS [Total Trips]
+         COUNT(t.FlightId) AS [Total Trips]
     FROM Passengers AS p
 FULL JOIN Tickets AS t
       ON t.PassengerId = p.Id
@@ -8,4 +8,4 @@ GROUP BY p.FirstName,
          p.LastName
 ORDER BY [Total Trips] DESC,
          p.FirstName,
-		 p.LastName
+         p.LastName
