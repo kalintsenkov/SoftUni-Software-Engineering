6dbbcbbd714e940d39a8cd6e833305678cee417f
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-08 20:11:03 +0300

    Update 13CashInUserGamesOddRows.sql

--- CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/13CashInUserGamesOddRows.sql
+++ CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/13CashInUserGamesOddRows.sql
@@ -5,10 +5,10 @@ RETURN
 	SELECT SUM(r.Cash) AS [SumCash]
 	  FROM (SELECT g.[Name],
 	               us.Cash,
-	  	           ROW_NUMBER() OVER (PARTITION BY @gameName ORDER BY us.Cash DESC)
-	  		    AS [Row Number]
+	  	       ROW_NUMBER() OVER (PARTITION BY @gameName ORDER BY us.Cash DESC)
+	            AS [Row Number]
 	          FROM Games AS g
 	          JOIN UsersGames AS us
 	            ON us.GameId = g.Id
-			 WHERE g.[Name] = @gameName) AS r
+	         WHERE g.[Name] = @gameName) AS r
 	  WHERE r.[Row Number] % 2 != 0
