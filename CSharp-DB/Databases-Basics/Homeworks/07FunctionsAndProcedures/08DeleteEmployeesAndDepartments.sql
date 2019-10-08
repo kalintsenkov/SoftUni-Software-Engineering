2c4b2ee37ed4eeeb25f6e80d54e9f0d6b4315536
Kalin Tsenkov <kalintsenkov3@gmail.com>
2019-10-08 21:13:31 +0300

    Add 08DeleteEmployeesAndDepartments.sql to 07FunctionsAndProcedures

---
+++ CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/08DeleteEmployeesAndDepartments.sql
@@ -0,0 +1,34 @@
+CREATE PROC usp_DeleteEmployeesFromDepartment (@departmentId INT) 
+AS
+BEGIN
+	DELETE FROM EmployeesProjects
+	WHERE EmployeeID IN (
+		SELECT EmployeeID
+		  FROM Employees
+		 WHERE DepartmentID = @departmentId
+	)
+
+	UPDATE Employees
+	   SET ManagerID = NULL
+	 WHERE ManagerID IN ( 
+		SELECT EmployeeID
+		  FROM Employees
+		 WHERE DepartmentID = @departmentId)
+
+	ALTER TABLE Departments
+	ALTER COLUMN ManagerId INT
+
+	UPDATE Departments
+	   SET ManagerID = NULL
+	 WHERE DepartmentID = @departmentId
+
+	 DELETE FROM Employees
+	 WHERE DepartmentID = @departmentId
+
+	 DELETE FROM Departments
+	 WHERE DepartmentID = @departmentId
+
+	 SELECT COUNT(*)
+	   FROM Employees
+      WHERE DepartmentID = @departmentId
+END
