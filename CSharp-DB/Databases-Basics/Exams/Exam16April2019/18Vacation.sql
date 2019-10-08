3e3584a844b7c6a7b35ebb256eff88e606d20537
Kalin Tsenkov <kalintsenkov3@gmail.com>
2019-10-08 20:04:35 +0300

    Add 18 and 19 solutions to Exam16April2019

---
+++ CSharp-DB/Databases-Basics/Exams/Exam16April2019/18Vacation.sql
@@ -0,0 +1,35 @@
+CREATE FUNCTION udf_CalculateTickets(@origin NVARCHAR(MAX), @destination NVARCHAR(MAX), @peopleCount INT)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @count INT 
+    DECLARE @sum DECIMAL(15, 2)
+
+	IF (@peopleCount <= 0)
+	BEGIN
+		RETURN 'Invalid people count!'
+	END
+
+	SET @count = ( SELECT COUNT(*)
+				     FROM Tickets AS t
+				     JOIN Flights AS f
+				       ON f.Id = t.FlightId
+				    WHERE f.Origin = @origin
+				      AND f.Destination = @destination)
+
+    IF (@count = 0)
+	BEGIN
+		RETURN 'Invalid flight!'
+	END
+	
+	SET @sum = ( SELECT t.Price
+				   FROM Tickets AS t
+				   JOIN Flights AS f
+				     ON f.Id = t.FlightId
+				  WHERE f.Origin = @origin
+				    AND f.Destination = @destination)
+
+    SET @sum *= @peopleCount
+
+	RETURN 'Total price ' + CAST(@sum AS NVARCHAR)
+END
---
+++ CSharp-DB/Databases-Basics/Exams/Exam16April2019/19WrongData.sql
@@ -0,0 +1,8 @@
+CREATE PROC usp_CancelFlights
+AS
+BEGIN
+	UPDATE Flights
+       SET ArrivalTime = NULL, 
+	       DepartureTime = NULL
+     WHERE ArrivalTime > DepartureTime
+END
