ca1a250bc62bcf482a4174edd4cd8cd436a1d834
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-08 20:08:59 +0300

    Update 04EmployeesFromTown.sql

--- CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/04EmployeesFromTown.sql
+++ CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/04EmployeesFromTown.sql
@@ -2,11 +2,11 @@ CREATE PROC usp_GetEmployeesFromTown(@town NVARCHAR(MAX))
 AS
 BEGIN
 	SELECT e.FirstName,
-           e.LastName
-      FROM Employees AS e
-      JOIN Addresses AS a
-        ON a.AddressID = e.AddressID
-      JOIN Towns AS t
-        ON t.TownID = a.TownID
+               e.LastName
+          FROM Employees AS e
+          JOIN Addresses AS a
+            ON a.AddressID = e.AddressID
+          JOIN Towns AS t
+            ON t.TownID = a.TownID
 	 WHERE t.[Name] = @town
 END
