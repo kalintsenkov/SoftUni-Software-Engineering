6b2d5da666b7e360e724e3fab696e8f06074eedb
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-08 20:09:51 +0300

    Update 10PeopleWithBalanceHigherThan.sql

--- CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/10PeopleWithBalanceHigherThan.sql
+++ CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/10PeopleWithBalanceHigherThan.sql
@@ -2,13 +2,13 @@ CREATE PROC usp_GetHoldersWithBalanceHigherThan(@number DECIMAL(15, 2))
 AS
 BEGIN
 	  SELECT ah.FirstName,
-			 ah.LastName
+		 ah.LastName
 	    FROM AccountHolders AS ah
 	    JOIN Accounts AS a
 	      ON a.AccountHolderId = ah.Id
 	GROUP BY ah.FirstName,
-			 ah.LastName
+		 ah.LastName
 	  HAVING SUM(a.Balance) > @number
 	ORDER BY ah.FirstName,
-			 ah.LastName
+		 ah.LastName
 END
