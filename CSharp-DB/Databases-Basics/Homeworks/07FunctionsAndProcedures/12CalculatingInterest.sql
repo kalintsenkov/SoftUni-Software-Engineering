9e0d582ee65d6f6a46609d6f9f1287fd17c9dded
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-08 20:10:22 +0300

    Update 12CalculatingInterest.sql

--- CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/12CalculatingInterest.sql
+++ CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/12CalculatingInterest.sql
@@ -2,11 +2,11 @@ CREATE PROC usp_CalculateFutureValueForAccount(@accountId INT, @interestRate FLO
 AS
 BEGIN
 	SELECT ah.Id AS [Account Id],
-		   ah.FirstName AS [First Name],
-		   ah.LastName AS [Last Name],
-		   a.Balance AS [Current Balance],
-		   dbo.ufn_CalculateFutureValue(a.Balance, @interestRate, 5)
-		AS [Balance in 5 years]
+	       ah.FirstName AS [First Name],
+	       ah.LastName AS [Last Name],
+	       a.Balance AS [Current Balance],
+	       dbo.ufn_CalculateFutureValue(a.Balance, @interestRate, 5)
+	    AS [Balance in 5 years]
 	  FROM AccountHolders AS ah
 	  JOIN Accounts AS a
 	    ON a.AccountHolderId = ah.Id
