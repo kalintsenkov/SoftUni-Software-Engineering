cb6bbec7829cfa0cc85a708c4f5da72f1a0d1cbb
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 10:38:41 +0300

    Update 14CreateTableLogs.sql

--- CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/14CreateTableLogs.sql
+++ CSharp-DB/Databases-Basics/Homeworks/07FunctionsAndProcedures/14CreateTableLogs.sql
@@ -6,13 +6,13 @@ BEGIN
 	DECLARE @newSum DECIMAL(15, 2);
 
 	SET @accountId = (SELECT i.Id
-				        FROM inserted AS i)
+		            FROM inserted AS i)
 
 	SET @oldSum = (SELECT d.Balance
-					 FROM deleted AS d)
+		         FROM deleted AS d)
 
 	SET @newSum = (SELECT i.Balance
-					 FROM inserted AS i)
+		         FROM inserted AS i)
 
 	INSERT INTO Logs(AccountId, OldSum, NewSum)
 	VALUES		(@accountId, @oldSum, @newSum)
