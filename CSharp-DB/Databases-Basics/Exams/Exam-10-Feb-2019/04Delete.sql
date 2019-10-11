25948b925c9bbd046d04e5aecd02b91524671491
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 19:55:38 +0300

    Update 04Delete.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/04Delete.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/04Delete.sql
@@ -1,5 +1,5 @@
 DELETE FROM TravelCards
-	  WHERE JourneyId IN (SELECT TOP(3) j.Id
-				FROM Journeys AS j)
+      WHERE JourneyId IN (SELECT TOP(3) j.Id
+		            FROM Journeys AS j)
 
 DELETE TOP(3) FROM Journeys
