ff63cb2897be2b23490f9b21d113b3fe83ac690f
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 10:43:01 +0300

    Update 12TopStudents.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/12TopStudents.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/12TopStudents.sql
@@ -1,11 +1,11 @@
   SELECT TOP(10) s.FirstName,
-		 s.LastName,
-		 CONVERT(DECIMAL(3, 2), AVG(se.Grade)) AS [Grade]
+	 s.LastName,
+	 CONVERT(DECIMAL(3, 2), AVG(se.Grade)) AS [Grade]
     FROM Students AS s
     JOIN StudentsExams AS se
       ON se.StudentId = s.Id
 GROUP BY s.FirstName, 
-		 s.LastName
+	 s.LastName
 ORDER BY [Grade] DESC,
-		 s.FirstName,
-		 s.LastName
+	 s.FirstName,
+	 s.LastName
