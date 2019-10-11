897fc08eab97733a11717887faa6df236b29ab66
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 20:08:28 +0300

    Update 11SelectSpaceshipsWithPilots.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/11SelectSpaceshipsWithPilots.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/11SelectSpaceshipsWithPilots.sql
@@ -1,7 +1,7 @@
   SELECT s.[Name], s.Manufacturer
     FROM Spaceships AS s
     JOIN Journeys AS j
-  	  ON j.SpaceshipId = s.Id
+      ON j.SpaceshipId = s.Id
     JOIN TravelCards AS tc
       ON tc.JourneyId = j.Id
     JOIN Colonists AS c
