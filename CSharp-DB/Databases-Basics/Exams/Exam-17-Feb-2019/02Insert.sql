b3dc1aee466a6c60fdc9952afa20960b49736889
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 10:44:22 +0300

    Update 02Insert.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/02Insert.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/02Insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO Teachers(FirstName, LastName, [Address], Phone, SubjectId)
-VALUES		('Ruthanne', 'Bamb', '84948 Mesta Junction', '3105500146', 6),
-			('Gerrard', 'Lowin', '370 Talisman Plaza', '3324874824', 2),
-			('Merrile', 'Lambdin', '81 Dahle Plaza', '4373065154', 5),
-			('Bert', 'Ivie', '2 Gateway Circle', '4409584510', 4)
+VALUES	    ('Ruthanne', 'Bamb', '84948 Mesta Junction', '3105500146', 6),
+	    ('Gerrard', 'Lowin', '370 Talisman Plaza', '3324874824', 2),
+	    ('Merrile', 'Lambdin', '81 Dahle Plaza', '4373065154', 5),
+	    ('Bert', 'Ivie', '2 Gateway Circle', '4409584510', 4)
 
 INSERT INTO Subjects([Name], Lessons)
-VALUES		('Geometry', 12),
-			('Health', 10),
-			('Drama', 7),
-			('Sports', 9)
+VALUES	    ('Geometry', 12),
+	    ('Health', 10),
+	    ('Drama', 7),
+	    ('Sports', 9)
