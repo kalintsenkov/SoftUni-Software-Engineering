3ff78be6c8313620a87ca113cecea5bf50c66784
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 19:54:55 +0300

    Update 02Insert.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/02Insert.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/02Insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO Planets([Name])
-VALUES		('Mars'),
-			('Earth'),
-			('Jupiter'),
-			('Saturn')
+VALUES	    ('Mars'),
+	    ('Earth'),
+	    ('Jupiter'),
+	    ('Saturn')
 			
 INSERT INTO Spaceships([Name], Manufacturer, LightSpeedRate)
-VALUES		('Golf', 'VW', 3),
-			('WakaWaka', 'Wakanda', 4),
-			('Falcon9', 'SpaceX', 1),
-			('Bed', 'Vidolov', 6)
+VALUES	    ('Golf', 'VW', 3),
+	    ('WakaWaka', 'Wakanda', 4),
+	    ('Falcon9', 'SpaceX', 1),
+	    ('Bed', 'Vidolov', 6)
