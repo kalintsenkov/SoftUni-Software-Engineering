a3b185636a509ce5438ce17406f9c8ecd1b852fc
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 10:45:21 +0300

    Update 06CoolAddresses.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/06CoolAddresses.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/06CoolAddresses.sql
@@ -1,7 +1,7 @@
   SELECT CONCAT(s.FirstName, ' ', s.MiddleName, ' ', s.LastName) AS [Full Name], 
-		 s.[Address]
+	 s.[Address]
     FROM Students AS s
    WHERE s.[Address] LIKE '%road%'
 ORDER BY s.FirstName, 
-	     s.LastName, 
-		 s.[Address]
+	 s.LastName, 
+         s.[Address]
