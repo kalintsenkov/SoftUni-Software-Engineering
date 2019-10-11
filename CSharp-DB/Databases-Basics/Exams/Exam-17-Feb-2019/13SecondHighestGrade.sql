7c78b1af31edd20c56ac53d2e963e715b0e6bbaa
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 10:42:05 +0300

    Update 13SecondHighestGrade.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/13SecondHighestGrade.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/13SecondHighestGrade.sql
@@ -1,13 +1,13 @@
    SELECT r.FirstName,
-   	      r.LastName,
-   	      r.Grade
+   	  r.LastName,
+   	  r.Grade
      FROM (SELECT s.FirstName,
-   		     	  s.LastName,
-   		     	  ss.Grade,
-   		     	  ROW_NUMBER() OVER (PARTITION BY s.FirstName, s.LastName ORDER BY ss.Grade DESC) 
-			   AS [RowNum]
-   		     FROM Students AS s
-   		     JOIN StudentsSubjects AS ss
-   		       ON ss.StudentId = s.Id) AS r
+   		  s.LastName,
+   		  ss.Grade,
+   		  ROW_NUMBER() OVER (PARTITION BY s.FirstName, s.LastName ORDER BY ss.Grade DESC) 
+               AS [RowNum]
+   	     FROM Students AS s
+   	     JOIN StudentsSubjects AS ss
+   	       ON ss.StudentId = s.Id) AS r
    WHERE r.RowNum = 2
 ORDER BY r.FirstName, r.LastName
