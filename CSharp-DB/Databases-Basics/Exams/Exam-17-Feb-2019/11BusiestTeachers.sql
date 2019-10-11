2aa02af3c20174e6fb6dda8d8bdf748f99a28879
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 10:43:31 +0300

    Update 11BusiestTeachers.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/11BusiestTeachers.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/11BusiestTeachers.sql
@@ -1,10 +1,10 @@
   SELECT TOP(10) t.FirstName,
-		 t.LastName,
-		 COUNT(st.StudentId) AS [StudentsCount]
+	 t.LastName,
+	 COUNT(st.StudentId) AS [StudentsCount]
     FROM StudentsTeachers AS st
     JOIN Teachers AS t
       ON t.Id = st.TeacherId
 GROUP BY t.FirstName, t.LastName
 ORDER BY [StudentsCount] DESC,
-		 t.FirstName,
-		 t.LastName
+	 t.FirstName,
+	 t.LastName
