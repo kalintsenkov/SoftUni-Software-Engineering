31fabb862cca44f61b87791d7e78a87df5daaaa6
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-11 10:46:03 +0300

    Update 09SubjectsWithStudents.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/09SubjectsWithStudents.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/09SubjectsWithStudents.sql
@@ -1,15 +1,15 @@
   SELECT CONCAT(t.FirstName, ' ', t.LastName) AS [FullName],
-		 CONCAT(s.[Name], '-', s.Lessons) AS [Subjects],
-		 COUNT(st.StudentId) AS [Students]
+	 CONCAT(s.[Name], '-', s.Lessons) AS [Subjects],
+	 COUNT(st.StudentId) AS [Students]
     FROM Teachers AS t
     JOIN Subjects AS s
       ON s.Id = t.SubjectId
     JOIN StudentsTeachers AS st
       ON st.TeacherId = t.Id
 GROUP BY t.FirstName,
-		 t.LastName,
-		 s.[Name],
-		 s.Lessons
+	 t.LastName,
+	 s.[Name],
+	 s.Lessons
 ORDER BY [Students] DESC,
-		 [FullName],
-		 [Subjects]
+	 [FullName],
+	 [Subjects]
