2367f635759a77998049630e22a4d2d72e62d92f
kalintsenkov <kalintsenkov3@gmail.com>
2019-10-11 11:23:26 +0300

    Add 20DeletedStudents.sql to Exam-17-Feb-2019

---
+++ CSharp-DB/Databases-Basics/Exams/Exam-17-Feb-2019/20DeletedStudents.sql
@@ -0,0 +1,8 @@
+CREATE TRIGGER tr_DeletedStudents ON Students FOR DELETE
+AS
+BEGIN
+	INSERT INTO ExcludedStudents(StudentId, StudentName)
+	     SELECT d.Id,
+				d.FirstName + ' ' + d.LastName
+		   FROM deleted AS d
+END
