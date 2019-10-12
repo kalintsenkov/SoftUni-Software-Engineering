73d9d4641fe4fe9c9efadde68ca0c4c2a3da26cb
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-12 16:32:49 +0300

    Update 16SelectSpecialColonists.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/16SelectSpecialColonists.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-10-Feb-2019/16SelectSpecialColonists.sql
@@ -1,10 +1,10 @@
 SELECT r.JobDuringJourney,
        r.FullName,
-	   r.JobRank
+       r.JobRank
   FROM (SELECT tc.JobDuringJourney,
                CONCAT(c.FirstName, ' ', c.LastName) AS [FullName],
-        	   ROW_NUMBER() OVER (PARTITION BY tc.JobDuringJourney ORDER BY DATEDIFF(MONTH, c.BirthDate, GETDATE()) DESC)
-        	AS [JobRank]
+               ROW_NUMBER() OVER (PARTITION BY tc.JobDuringJourney ORDER BY DATEDIFF(MONTH, c.BirthDate, GETDATE()) DESC)
+            AS [JobRank]
           FROM TravelCards AS tc
           JOIN Colonists AS c
             ON c.Id = tc.ColonistId) AS r
