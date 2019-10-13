40914d25072cd0f91e202ff7e7283f02c6af94da
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-13 11:13:26 +0300

    Update 09MostContributedRepositories.sql

--- CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/09MostContributedRepositories.sql
+++ CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/09MostContributedRepositories.sql
@@ -1,12 +1,12 @@
   SELECT TOP(5) r.Id, 
          r.[Name],
-		 COUNT(c.Id) AS [Commits]
+	 COUNT(c.Id) AS [Commits]
     FROM RepositoriesContributors AS rc
     JOIN Repositories AS r
       ON r.Id = rc.RepositoryId
-	JOIN Commits AS c
-	  ON c.RepositoryId = r.Id
+    JOIN Commits AS c
+      ON c.RepositoryId = r.Id
 GROUP BY r.Id, r.[Name]
 ORDER BY [Commits] DESC,
          r.Id,
-		 r.[Name]
+	 r.[Name]
