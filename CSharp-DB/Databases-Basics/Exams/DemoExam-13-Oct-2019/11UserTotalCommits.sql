633e9ec13afee1413911c3b3ca1db0f44593e96a
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-13 11:14:24 +0300

    Update 11UserTotalCommits.sql

--- CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/11UserTotalCommits.sql
+++ CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/11UserTotalCommits.sql
@@ -4,9 +4,9 @@ AS
 BEGIN
 	DECLARE @count INT = (SELECT COUNT(c.Id)
 	                        FROM Commits AS c
-							JOIN Users AS u
-							  ON u.Id = c.ContributorId
-						   WHERE u.Username = @username)
+				JOIN Users AS u
+				  ON u.Id = c.ContributorId
+			       WHERE u.Username = @username)
 
 	RETURN @count
 END
