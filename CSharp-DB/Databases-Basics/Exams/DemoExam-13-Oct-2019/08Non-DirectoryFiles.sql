a394815d284557b498f77a050ef0677a007b8319
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-13 11:13:00 +0300

    Update 08Non-DirectoryFiles.sql

--- CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/08Non-DirectoryFiles.sql
+++ CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/08Non-DirectoryFiles.sql
@@ -1,8 +1,8 @@
    SELECT f.Id,
           f.[Name],
-  	      CONCAT(f.Size, 'KB') AS [Size]
+  	  CONCAT(f.Size, 'KB') AS [Size]
      FROM Files AS f
 LEFT JOIN Files AS p
        ON p.ParentId = f.Id
-	WHERE p.Id IS NULL
+    WHERE p.Id IS NULL
  ORDER BY f.Id, f.[Name], f.Size DESC
