47a70934c4628e94d89a89a17a8506a8d99891b1
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-13 11:12:06 +0300

    Update 04Delete.sql

--- CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/04Delete.sql
+++ CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/04Delete.sql
@@ -1,9 +1,9 @@
 DELETE FROM RepositoriesContributors
       WHERE RepositoryId IN (SELECT r.Id
-	                           FROM Repositories AS r
-							  WHERE r.[Name] = 'Softuni-Teamwork')
+	                       FROM Repositories AS r
+			      WHERE r.[Name] = 'Softuni-Teamwork')
 
 DELETE FROM Issues
       WHERE RepositoryId IN (SELECT r.Id
-	                           FROM Repositories AS r
-							  WHERE r.[Name] = 'Softuni-Teamwork')
+	                       FROM Repositories AS r
+			      WHERE r.[Name] = 'Softuni-Teamwork')
