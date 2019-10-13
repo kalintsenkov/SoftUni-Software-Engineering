8eee3badc555e34bcc0fffc72acf0bc356254606
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-13 11:13:54 +0300

    Update 10UserAndFiles.sql

--- CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/10UserAndFiles.sql
+++ CSharp-DB/Databases-Basics/Exams/DemoExam-13-Oct-2019/10UserAndFiles.sql
@@ -1,10 +1,10 @@
   SELECT u.Username,
-		 AVG(f.Size) AS [Size]
+	 AVG(f.Size) AS [Size]
     FROM Commits AS c
     JOIN Users AS u
       ON u.Id = c.ContributorId
-	JOIN Files AS f
-	  ON f.CommitId = c.Id
+    JOIN Files AS f
+      ON f.CommitId = c.Id
 GROUP BY u.Username
 ORDER BY AVG(f.Size) DESC,
          u.Username
