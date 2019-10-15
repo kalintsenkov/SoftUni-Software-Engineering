9625c306354f0c0b1d1a54986a6a8daf8868c83a
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-15 10:25:09 +0300

    Update 02Insert.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/02Insert.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/02Insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO Employees(FirstName, LastName, Phone, Salary)
-VALUES		('Stoyan', 'Petrov', '888-785-8573', 500.25),
-			('Stamat', 'Nikolov', '789-613-1122', 999995.25),
-			('Evgeni', 'Petkov', '645-369-9517', 1234.51),
-			('Krasimir', 'Vidolov', '321-471-9982', 50.25)
+VALUES	    ('Stoyan', 'Petrov', '888-785-8573', 500.25),
+	    ('Stamat', 'Nikolov', '789-613-1122', 999995.25),
+	    ('Evgeni', 'Petkov', '645-369-9517', 1234.51),
+	    ('Krasimir', 'Vidolov', '321-471-9982', 50.25)
 
 INSERT INTO Items([Name], Price, CategoryId)
-VALUES		('Tesla battery', 154.25, 8),
-			('Chess', 30.25, 8),
-			('Juice', 5.32, 1),
-			('Glasses', 10, 8),
-			('Bottle of water', 1, 1)
+VALUES	    ('Tesla battery', 154.25, 8),
+	    ('Chess', 30.25, 8),
+	    ('Juice', 5.32, 1),
+	    ('Glasses', 10, 8),
+	    ('Bottle of water', 1, 1)
