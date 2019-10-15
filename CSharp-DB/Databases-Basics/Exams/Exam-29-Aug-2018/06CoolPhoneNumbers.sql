0b9e0d931facd86b2aeb6d2cb13cc3a3a19a5121
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-15 10:25:46 +0300

    Update 06CoolPhoneNumbers.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/06CoolPhoneNumbers.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/06CoolPhoneNumbers.sql
@@ -1,5 +1,5 @@
   SELECT CONCAT(e.FirstName, ' ', e.LastName) AS [Full Name],
-  	     e.Phone AS [Phone Number]
+         e.Phone AS [Phone Number]
     FROM Employees AS e
    WHERE e.Phone LIKE '3%'
 ORDER BY e.FirstName, e.Phone
