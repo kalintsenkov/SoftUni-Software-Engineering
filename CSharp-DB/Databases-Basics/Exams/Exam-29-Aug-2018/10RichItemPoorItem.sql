d165a210438dc2f1ad83f769522bd79e759259fb
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-16 15:03:08 +0300

    Update 10RichItemPoorItem.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/10RichItemPoorItem.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/10RichItemPoorItem.sql
@@ -1,6 +1,6 @@
   SELECT TOP(10) oi.OrderId,
-		 MAX(i.Price) AS [ExpensivePrice],
-		 MIN(i.Price) AS [CheapPrice]
+	 MAX(i.Price) AS [ExpensivePrice],
+	 MIN(i.Price) AS [CheapPrice]
     FROM OrderItems AS oi
     JOIN Items AS i
       ON i.Id = oi.ItemId
