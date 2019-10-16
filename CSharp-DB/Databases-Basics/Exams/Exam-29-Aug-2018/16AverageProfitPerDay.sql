57f596986e0086c8e1c50fba03a443cdb22c4e76
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-16 15:05:49 +0300

    Update 16AverageProfitPerDay.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/16AverageProfitPerDay.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/16AverageProfitPerDay.sql
@@ -1,9 +1,9 @@
   SELECT DATEPART(DAY, o.[DateTime]) AS [Day],
-  	     CAST(AVG(i.Price * oi.Quantity) AS DECIMAL(15, 2)) AS [Total profit]
+  	 CAST(AVG(i.Price * oi.Quantity) AS DECIMAL(15, 2)) AS [Total profit]
     FROM OrderItems AS oi
     JOIN Orders AS o
       ON o.Id = oi.OrderId
-	JOIN Items AS i
-	  ON i.Id = oi.ItemId
+    JOIN Items AS i
+      ON i.Id = oi.ItemId
 GROUP BY DATEPART(DAY, o.[DateTime])
 ORDER BY [Day]
