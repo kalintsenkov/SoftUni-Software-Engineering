a560c52906a4aadea2b2f56bb61937c9fc42c454
Kalin Tsenkov <kalintsenkov3@gmail.com>
2019-10-16 15:01:36 +0300

    Add solutions to Exam-29-Aug-2018

---
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/19CancelOrder.sql
@@ -0,0 +1,31 @@
+CREATE PROC usp_CancelOrder(@orderId INT, @cancelDate DATE)
+AS
+BEGIN TRANSACTION
+	DECLARE @issueDate DATE;
+
+	IF (NOT EXISTS(
+	    SELECT * FROM Orders
+		 WHERE Id = @orderId))
+	BEGIN
+		ROLLBACK
+		RAISERROR('The order does not exist!', 16, 1)
+		RETURN
+	END
+
+	SET @issueDate = (SELECT o.[DateTime]
+	                    FROM Orders AS o
+					   WHERE o.Id = @orderId)
+
+	IF (DATEDIFF(DAY, @issueDate, @cancelDate) > 3)
+	BEGIN
+		ROLLBACK
+		RAISERROR('You cannot cancel the order!', 16, 2)
+		RETURN
+	END
+
+	DELETE FROM OrderItems
+	      WHERE OrderId = @orderId
+	
+	DELETE FROM Orders
+	      WHERE Id = @orderId
+COMMIT
---
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/20DeletedOrders.sql
@@ -0,0 +1,7 @@
+CREATE TRIGGER tr_DeletedOrders ON OrderItems FOR DELETE
+AS
+BEGIN
+	INSERT INTO DeletedOrders(OrderId, ItemId, ItemQuantity)
+		 SELECT d.OrderId, d.ItemId, d.Quantity
+		   FROM deleted AS d
+END
