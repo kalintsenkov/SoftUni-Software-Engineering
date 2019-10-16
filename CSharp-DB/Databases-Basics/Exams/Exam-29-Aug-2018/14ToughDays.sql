a5193eb6d60f6e29e6ef644f14edef4ba86efcd4
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-16 15:04:12 +0300

    Update 14ToughDays.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/14ToughDays.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/14ToughDays.sql
@@ -1,12 +1,12 @@
    SELECT CONCAT(e.FirstName, ' ', e.LastName) AS [Full Name],
      CASE
-		  WHEN DATEPART(WEEKDAY, s.CheckIn) = 1 THEN 'Sunday'
-		  WHEN DATEPART(WEEKDAY, s.CheckIn) = 2 THEN 'Monday'
-		  WHEN DATEPART(WEEKDAY, s.CheckIn) = 3 THEN 'Tuesday'
-		  WHEN DATEPART(WEEKDAY, s.CheckIn) = 4 THEN 'Wednesday'
-		  WHEN DATEPART(WEEKDAY, s.CheckIn) = 5 THEN 'Thursday'
-		  WHEN DATEPART(WEEKDAY, s.CheckIn) = 6 THEN 'Friday'
-		  WHEN DATEPART(WEEKDAY, s.CheckIn) = 7 THEN 'Saturday'
+	  WHEN DATEPART(WEEKDAY, s.CheckIn) = 1 THEN 'Sunday'
+	  WHEN DATEPART(WEEKDAY, s.CheckIn) = 2 THEN 'Monday'
+	  WHEN DATEPART(WEEKDAY, s.CheckIn) = 3 THEN 'Tuesday'
+	  WHEN DATEPART(WEEKDAY, s.CheckIn) = 4 THEN 'Wednesday'
+	  WHEN DATEPART(WEEKDAY, s.CheckIn) = 5 THEN 'Thursday'
+	  WHEN DATEPART(WEEKDAY, s.CheckIn) = 6 THEN 'Friday'
+	  WHEN DATEPART(WEEKDAY, s.CheckIn) = 7 THEN 'Saturday'
    END AS [Day of week]
      FROM Employees AS e
 LEFT JOIN Orders AS o
@@ -14,5 +14,5 @@ LEFT JOIN Orders AS o
 LEFT JOIN Shifts AS s
        ON s.EmployeeId = e.Id
     WHERE o.Id IS NULL
-	  AND DATEDIFF(HOUR, s.CheckIn, s.CheckOut) > 12
+      AND DATEDIFF(HOUR, s.CheckIn, s.CheckOut) > 12
  ORDER BY e.Id
