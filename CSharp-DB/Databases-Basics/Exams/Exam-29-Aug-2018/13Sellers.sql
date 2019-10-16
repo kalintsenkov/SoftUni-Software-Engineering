d0059251aec420e7a526e7c75eb8c44e16a1c23b
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-16 15:03:44 +0300

    Update 13Sellers.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/13Sellers.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/13Sellers.sql
@@ -1,9 +1,9 @@
 SELECT TOP(10) k.[Full Name],
        SUM(k.Price) AS [Total Price],
-	   SUM(k.Items) AS [Items]
+       SUM(k.Items) AS [Items]
   FROM (SELECT CONCAT(e.FirstName, ' ', e.LastName) AS [Full Name],
                i.Price * oi.Quantity AS [Price],
-          	   oi.Quantity AS [Items]
+               oi.Quantity AS [Items]
           FROM OrderItems AS oi
           JOIN Items AS i
             ON i.Id = oi.ItemId
@@ -11,6 +11,6 @@ SELECT TOP(10) k.[Full Name],
             ON o.Id = oi.OrderId
           JOIN Employees AS e
             ON e.Id = o.EmployeeId
-		 WHERE o.[DateTime] < '2018-06-15') AS k
+         WHERE o.[DateTime] < '2018-06-15') AS k
 GROUP BY k.[Full Name]
 ORDER BY [Total Price] DESC, [Items] DESC
