4c5ddbbebb3cf47b1b8da727b78ec06d8fdf5a17
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-16 15:06:54 +0300

    Update 20DeletedOrders.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/20DeletedOrders.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-29-Aug-2018/20DeletedOrders.sql
@@ -2,6 +2,6 @@ CREATE TRIGGER tr_DeletedOrders ON OrderItems FOR DELETE
 AS
 BEGIN
 	INSERT INTO DeletedOrders(OrderId, ItemId, ItemQuantity)
-		 SELECT d.OrderId, d.ItemId, d.Quantity
-		   FROM deleted AS d
+	     SELECT d.OrderId, d.ItemId, d.Quantity
+	       FROM deleted AS d
 END
