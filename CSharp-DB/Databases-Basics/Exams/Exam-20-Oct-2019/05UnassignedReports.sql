8c7eb191bb449ed971465790d7b41a9ccb5f521d
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-20 18:47:08 +0300

    Update 05UnassignedReports.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-20-Oct-2019/05UnassignedReports.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-20-Oct-2019/05UnassignedReports.sql
@@ -1,5 +1,5 @@
   SELECT r.[Description],
-	     FORMAT(r.OpenDate, 'dd-MM-yyyy') AS [OpenDate]
+	 FORMAT(r.OpenDate, 'dd-MM-yyyy') AS [OpenDate]
     FROM Reports AS r
    WHERE r.EmployeeId IS NULL
 ORDER BY r.OpenDate, r.[Description]
