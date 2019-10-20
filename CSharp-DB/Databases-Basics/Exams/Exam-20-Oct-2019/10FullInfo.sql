a9770a5ea8654c7f6897d1d4db187ad72adc9d08
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-10-20 18:48:32 +0300

    Update 10FullInfo.sql

--- CSharp-DB/Databases-Basics/Exams/Exam-20-Oct-2019/10FullInfo.sql
+++ CSharp-DB/Databases-Basics/Exams/Exam-20-Oct-2019/10FullInfo.sql
@@ -1,10 +1,10 @@
    SELECT IIF(e.FirstName IS NULL OR e.FirstName = '', 'None', CONCAT(e.FirstName, ' ', e.LastName)) AS [Employee],
           ISNULL(d.[Name], 'None') AS [Department],
-		  c.[Name] AS [Category],
-		  r.[Description],
-		  FORMAT(r.OpenDate, 'dd.MM.yyyy') AS [OpenDate],
-		  s.[Label] AS [Status],
-		  u.[Name] AS [User]
+	  c.[Name] AS [Category],
+	  r.[Description],
+	  FORMAT(r.OpenDate, 'dd.MM.yyyy') AS [OpenDate],
+	  s.[Label] AS [Status],
+	  u.[Name] AS [User]
      FROM Reports AS r
 LEFT JOIN Employees AS e
        ON e.Id = r.EmployeeId
@@ -18,9 +18,9 @@ LEFT JOIN Users AS u
        ON u.Id = r.UserId
  ORDER BY e.FirstName DESC, 
           e.LastName DESC,
-		  d.[Name], 
-		  c.[Name], 
-		  r.[Description],
-		  r.OpenDate, 
-		  s.[Label], 
-		  u.[Name]
+	  d.[Name], 
+	  c.[Name], 
+	  r.[Description],
+	  r.OpenDate, 
+	  s.[Label], 
+	  u.[Name]
