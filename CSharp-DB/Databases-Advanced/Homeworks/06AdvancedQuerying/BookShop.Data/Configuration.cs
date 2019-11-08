92f577c4f0baf6b0c516dae55c773fa1bb8e2d35
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-11-08 17:36:43 +0200

    Delete Configuration.cs

--- CSharp-DB/Databases-Advanced/Homeworks/06AdvancedQuerying/BookShop.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal const string ConnectionString 
-            = @"Server=KALIN-PC\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
-    }
-}
