28d9b9d2f05141c72cfdacc9e176ffd8f6fa50b4
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-12-05 15:00:50 +0200

    Update VaporStoreDbContext.cs

--- CSharp-DB/Databases-Advanced/Exams/Exam-01-Sept-2018/VaporStore/Data/VaporStoreDbContext.cs
+++ CSharp-DB/Databases-Advanced/Exams/Exam-01-Sept-2018/VaporStore/Data/VaporStoreDbContext.cs
@@ -2,9 +2,9 @@
 {
 	using Microsoft.EntityFrameworkCore;
 
-    using Models;
+    	using Models;
 
-    public class VaporStoreDbContext : DbContext
+    	public class VaporStoreDbContext : DbContext
 	{
 		public VaporStoreDbContext()
 		{
@@ -15,32 +15,32 @@
 		{
 		}
 
-        public DbSet<Card> Cards { get; set; }
-
-        public DbSet<Developer> Developers { get; set; }
-
-        public DbSet<Genre> Genres { get; set; }
-
-        public DbSet<Game> Games { get; set; }
-
-        public DbSet<Tag> Tags { get; set; }
-
-        public DbSet<GameTag> GameTags { get; set; }
-
-        public DbSet<Purchase> Purchases { get; set; }
-
-        public DbSet<User> Users { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        	public DbSet<Card> Cards { get; set; }
+	
+        	public DbSet<Developer> Developers { get; set; }
+	
+        	public DbSet<Genre> Genres { get; set; }
+	
+        	public DbSet<Game> Games { get; set; }
+	
+        	public DbSet<Tag> Tags { get; set; }
+	
+        	public DbSet<GameTag> GameTags { get; set; }
+	
+        	public DbSet<Purchase> Purchases { get; set; }
+	
+        	public DbSet<User> Users { get; set; }
+	
+        	protected override void OnConfiguring(DbContextOptionsBuilder options)
 		{
 			if (!options.IsConfigured)
 			{
 				options
-					.UseSqlServer(Configuration.ConnectionString);
+			 		.UseSqlServer(Configuration.ConnectionString);
 			}
 		}
 
-        protected override void OnModelCreating(ModelBuilder builder)
-            => builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        	protected override void OnModelCreating(ModelBuilder builder)
+            		=> builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
 	}
 }
