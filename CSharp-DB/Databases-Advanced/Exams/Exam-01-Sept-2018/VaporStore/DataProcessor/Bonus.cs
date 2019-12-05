7d18c86e60e3add7cff76e767247e21cda5444c7
Kalin Tsenkov <47757405+kalintsenkov@users.noreply.github.com>
2019-12-05 14:53:11 +0200

    Update Bonus.cs

--- CSharp-DB/Databases-Advanced/Exams/Exam-01-Sept-2018/VaporStore/DataProcessor/Bonus.cs
+++ CSharp-DB/Databases-Advanced/Exams/Exam-01-Sept-2018/VaporStore/DataProcessor/Bonus.cs
@@ -8,22 +8,22 @@
 	{
 		public static string UpdateEmail(VaporStoreDbContext context, string username, string newEmail)
 		{
-            var user = context.Users.FirstOrDefault(u => u.Username == username);
-
-            if (user == null)
-            {
-                return $"User {username} not found";
-            }
-
-            if (context.Users.Any(u => u.Email == newEmail))
-            {
-                return $"Email {newEmail} is already taken";
-            }
-
-            user.Email = newEmail;
-            context.SaveChanges();
-
-            return $"Changed {username}'s email successfully";
+            		var user = context.Users.FirstOrDefault(u => u.Username == username);
+		
+            		if (user == null)
+            		{
+            		    return $"User {username} not found";
+            		}
+		
+            		if (context.Users.Any(u => u.Email == newEmail))
+            		{
+            		    return $"Email {newEmail} is already taken";
+            		}
+		
+            		user.Email = newEmail;
+            		context.SaveChanges();
+		
+            		return $"Changed {username}'s email successfully";
 		}
 	}
 }
