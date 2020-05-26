541d954bc4cb5f16619bf38593570edc0c9e6565
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-05-26 14:40:28 +0300

    Fix exercise-10.js

--- JS-Advanced/Homeworks/02Arrays-Exercise/exercise-10.js
+++ JS-Advanced/Homeworks/02Arrays-Exercise/exercise-10.js
@@ -7,33 +7,32 @@ function solve(input) {
     const starRow = input[2];
     const starCol = input[3];
 
-    let counter = 1;
-
-    const fillValue = 0;
+    const fillValue = null;
     const matrix = new Array(rows).fill(fillValue).map(() => new Array(cols).fill(fillValue));
-    matrix[starRow][starCol] = counter;
+    matrix[starRow][starCol] = 1;
 
     let startRow = starRow - 1 < 0 ? starRow : starRow - 1;
     let startCol = starCol - 1 < 0 ? starCol : starCol - 1;
     let endRow = starRow + 1;
     let endCol = starCol + 1;
 
-    while (counter !== rows && counter !== cols) {
-
-        counter++;
+    for (let i = 2; i <= matrix.length; i++) {
 
         for (let row = startRow; row <= endRow; row++) {
             for (let col = startCol; col <= endCol; col++) {
-                if (row >= 0 && row < matrix.length && col >= 0 && col < matrix[row].length) {
-                    if (matrix[row][col] === fillValue) {
-                        matrix[row][col] = counter;
-                    }
+                
+                if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[row].length) {
+                    continue;
+                }
+
+                if (matrix[row][col] === fillValue) {
+                    matrix[row][col] = i;
                 }
             }
         }
 
         startRow = startRow - 1 < 0 ? startRow : startRow - 1;
-        startCol = startRow - 1 < 0 ? startRow : startRow - 1;
+        startCol = startCol - 1 < 0 ? startCol : startCol - 1;
 
         endRow++;
         endCol++;
