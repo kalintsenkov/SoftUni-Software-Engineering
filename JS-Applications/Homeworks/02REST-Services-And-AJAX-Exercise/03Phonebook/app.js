727bb0bf22935690b8eb1fe76480ed0523ac492c
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-07-10 17:44:39 +0300

    Add 03Phonebook/app.js

--- JS-Applications/Homeworks/02REST-Services-And-AJAX-Exercise/03Phonebook/app.js
+++ JS-Applications/Homeworks/02REST-Services-And-AJAX-Exercise/03Phonebook/app.js
@@ -1,5 +1,48 @@
+const url = 'http://localhost:3000/phonebook';
+
+const phonebookUl = document.getElementById('phonebook');
+const loadBtn = document.getElementById('btnLoad');
+const createBtn = document.getElementById('btnCreate');
+const personInput = document.getElementById('person');
+const phoneInput = document.getElementById('phone');
+
 function attachEvents() {
-    console.log('TODO...');
+    loadBtn.addEventListener('click', loadData);
+    createBtn.addEventListener('click', createData)
+
+    function loadData() {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                phonebookUl.innerHTML = '';
+
+                for (const id in data) {
+                    const { person, phone } = data[id];
+                    const li = document.createElement('li');
+                    li.innerHTML = `${person}: ${phone} `;
+                    phonebookUl.appendChild(li);
+                }
+            });
+    }
+
+    function createData() {
+        const data = {
+            person: personInput.value,
+            phone: phoneInput.value
+        };
+
+        personInput.value = '';
+        phoneInput.value = '';
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        .then(loadData())
+    }
 }
 
 attachEvents();
