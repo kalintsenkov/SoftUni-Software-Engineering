ada9ceca1955b6f5e9d0a17c98c0d52a02f0d8bb
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-07-21 07:27:13 +0300

    Finish 02. HTTP Status Cats

--- JS-Applications/Homeworks/05Templating-Exercise/02. HTTP Status Cats/template.js
+++ JS-Applications/Homeworks/05Templating-Exercise/02. HTTP Status Cats/template.js
@@ -1,8 +1,36 @@
-(() => {
-     renderCatTemplate();
+(async () => {
+    const catCardTemplate = await fetch('./templates/cat-card.hbs');
+    const catCardTemplateString = await catCardTemplate.text();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+    const catsListTemplate = await fetch('./templates/cats-list.hbs');
+    const catsListTemplateString = await catsListTemplate.text();
+
+    Handlebars.registerPartial('cat', catCardTemplateString);
+    const template = Handlebars.compile(catsListTemplateString);
+
+    renderCatTemplate();
+
+    function renderCatTemplate() {
+        const catsListSection = document.getElementById('allCats');
+        catsListSection.innerHTML = template({ cats });
+
+        catsListSection.addEventListener('click', (e) => {
+            const target = e.target;
+
+            if (!target.classList.contains('showBtn')) {
+                return;
+            }
+
+            target.innerHTML = target.innerHTML === 'Show status code'
+                ? 'Hide status code'
+                : 'Show status code';
+
+            const statusDiv = target.nextElementSibling;
+            const style = statusDiv.style
+
+            style.display = style.display === 'none'
+                ? ''
+                : 'none';
+        });
+    }
+})();
